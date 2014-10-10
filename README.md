bd17c24799dcab6e3b874e3569c099cb9e901b36
leungmanhin <manhinleung@gmail.com>
2014-10-10 21:07:58 +0800

    modified to read config file

--- README.md
+++ README.md
@@ -8,20 +8,10 @@ Prerequisites
 Have the following installed / running on the computer that is going to compile and run ZenoDial:
 - JDK 7 or above
 - ant
-- rosjava indigo
-- itf_listen.py
-- itf_talk.py
 
 
 How to run it
 -------------
 Clone the project into your workspace, then execute the commands in sequence:
-
-ant
-
-ant ChatBot
-
-
-Note
-----
-You can communicate with ZenoDial via command line but currently that interface is a mess, so I don't recommand you to do so...
+- ant
+- ant ChatBot
---
+++ src/zenodial/ros/ROSListener.java
@@ -0,0 +1,23 @@
+package zenodial.ros;
+
+import org.ros.message.MessageListener;
+import org.ros.node.topic.Subscriber;
+import zenodial.log.Logger;
+import zenodial.log.Logger.Level;
+import zenodial.process.Input;
+
+public class ROSListener {
+	private static Logger log = new Logger("ROSListener", Level.NORMAL);
+	
+	public static void createInputListener() {
+		Subscriber<std_msgs.String> subscriber = ROSMaster.initializeSubscriber("ZenoDialListener", "zenodial_listen");
+		subscriber.addMessageListener(new MessageListener<std_msgs.String>() {
+			@Override
+			public void onNewMessage(std_msgs.String message) {
+				String msgReceived = message.getData();
+				log.info("Message reached: \"" + msgReceived + "\"");
+				Input.setInputUtterance(msgReceived);
+			}
+		});
+	}
+}
